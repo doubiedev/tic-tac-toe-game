cf18fe1176146ff2630307a1d266a52657dccb02
NickDoublet <39459905+NickDoublet@users.noreply.github.com>
2023-09-25 13:11:04 +1000

    add score component, renders based on props

---
+++ src/components/Score.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNewGameMenu } from '../contexts/NewGameMenuContext';
+import { useGame } from '../contexts/GameContext';
+
+const Score = ({ type }) => {
+	const { gameType, isPlayerOneX } = useNewGameMenu();
+	const { score } = useGame();
+	return (
+		<>
+			{console.log(type)}
+			{gameType === 'cpu' && (
+				<div className='score'>
+					<p className='body'>
+						{type.toUpperCase()}
+						{isPlayerOneX && type === 'x' && ' (YOU)'}
+						{isPlayerOneX && type === 'o' && ' (CPU)'}
+						{!isPlayerOneX && type === 'x' && ' (CPU)'}
+						{!isPlayerOneX && type === 'o' && ' (YOU)'}
+					</p>
+					<h2>{score[type]}</h2>
+				</div>
+			)}
+			{gameType === 'player' && (
+				<div className='score'>
+					<p className='body'>
+						{type.toUpperCase()}
+						{isPlayerOneX && type === 'x' && ' (P1)'}
+						{isPlayerOneX && type === 'o' && ' (P2)'}
+						{!isPlayerOneX && type === 'x' && ' (P2)'}
+						{!isPlayerOneX && type === 'o' && ' (P1)'}
+					</p>
+					<h2>{score[type]}</h2>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Score;
--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,47 +1,13 @@
 import React from 'react';
-import { useNewGameMenu } from '../contexts/NewGameMenuContext';
-import { useGame } from '../contexts/GameContext';
+import Score from './Score';
 
 const Scores = () => {
-	const { gameType, isPlayerOneX } = useNewGameMenu();
-	const { score } = useGame();
 	return (
-		<>
-			{gameType === 'cpu' && (
-				<div className='scores-container'>
-					<div className='score'>
-						<p>
-							X({isPlayerOneX ? 'YOU' : 'CPU'}): {score.x}
-						</p>
-					</div>
-					<div className='score'>
-						<p>TIES: {score.ties}</p>
-					</div>
-					<div className='score'>
-						<p>
-							O({isPlayerOneX ? 'CPU' : 'YOU'}): {score.o}
-						</p>
-					</div>
-				</div>
-			)}
-			{gameType === 'player' && (
-				<div className='scores-container'>
-					<div className='score'>
-						<p>
-							X({isPlayerOneX ? 'P1' : 'P2'}): {score.x}
-						</p>
-					</div>
-					<div className='score'>
-						<p>TIES: {score.ties}</p>
-					</div>
-					<div className='score'>
-						<p>
-							O({isPlayerOneX ? 'P2' : 'P1'}): {score.o}
-						</p>
-					</div>
-				</div>
-			)}
-		</>
+		<div className='scores-container'>
+			<Score type='x' />
+			<Score type='ties' />
+			<Score type='o' />
+		</div>
 	);
 };
 
