e70eb330dc2190b12c131b8af58b115a2bc3544b
NickDoublet <39459905+NickDoublet@users.noreply.github.com>
2023-09-29 17:19:48 +1000

    clean up banner buttons conditional rendering

--- src/components/BannerButtons.js
+++ src/components/BannerButtons.js
@@ -1,51 +1,31 @@
 import React from 'react';
 import { useGame, useGameUpdate } from '../contexts/GameContext';
 
-const BannerButtons = ({ type }) => {
+const BannerButtons = () => {
 	const { isGameOver } = useGame();
 	const { handleBannerReset, toggleBanner, handleQuit } = useGameUpdate();
 	return (
-		<div className='banner-btns'>
-			{isGameOver ? (
-				<div className='flex'>
-					<button
-						className='bg-light bg-light-hover bs-light-M text-dark br-M pointer h-max'
-						onClick={() => {
-							handleQuit();
-							toggleBanner();
-						}}
-					>
-						<h4>QUIT</h4>
-					</button>
-					<button
-						className='bg-yellow bg-yellow-hover bs-yellow-M text-dark br-M pointer h-max'
-						onClick={() => {
-							handleBannerReset();
-							toggleBanner();
-						}}
-					>
-						<h4>NEXT ROUND</h4>
-					</button>
-				</div>
-			) : (
-				<div className='flex'>
-					<button
-						className='bg-light bg-light-hover bs-light-M text-dark br-M pointer h-max'
-						onClick={() => toggleBanner()}
-					>
-						<h4>NO, CANCEL</h4>
-					</button>
-					<button
-						className='bg-yellow bg-yellow-hover bs-yellow-M text-dark br-M pointer h-max'
-						onClick={() => {
-							handleBannerReset();
-							toggleBanner();
-						}}
-					>
-						<h4>YES, RESTART</h4>
-					</button>
-				</div>
-			)}
+		<div className='banner-btns flex'>
+			<button
+				className='bg-light bg-light-hover bs-light-M text-dark br-M pointer h-max'
+				onClick={() => {
+					if (isGameOver) {
+						handleQuit();
+					}
+					toggleBanner();
+				}}
+			>
+				<h4>{`${isGameOver ? 'QUIT' : 'CANCEL'}`}</h4>
+			</button>
+			<button
+				className='bg-yellow bg-yellow-hover bs-yellow-M text-dark br-M pointer h-max'
+				onClick={() => {
+					handleBannerReset();
+					toggleBanner();
+				}}
+			>
+				<h4>{`${isGameOver ? 'NEXT ROUND' : 'RESTART'}`}</h4>
+			</button>
 		</div>
 	);
 };
